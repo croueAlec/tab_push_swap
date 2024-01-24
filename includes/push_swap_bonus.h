8e7bbad87699b19fc0f7332e230f546bf0ae9a1b
Alec Croue <acroue@bess-f1r1s2.clusters.42paris.fr>
2024-01-24 18:26:49 +0100

    checker first push

---
+++ includes/push_swap_bonus.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_bonus.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 13:10:49 by acroue            #+#    #+#             */
+/*   Updated: 2024/01/24 14:29:13 by acroue           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_BONUS_H
+# define PUSH_SWAP_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include "../libft/libft.h"
+
+typedef struct s_len
+{
+	size_t		a;
+	size_t		b;
+	size_t		total;
+}	t_len;
+
+typedef struct s_a
+{
+	int				value;
+	struct s_len	*len;
+}	t_a;
+
+/* checker_bonus.c */
+void	apply_checker(char **instructions, t_a *a, t_a *b);
+/* operators_bonus.c */
+void	swap(t_a *tab);
+void	super_swap(t_a *a, t_a *b);
+int		push_b(t_a *a, t_a *b, size_t len);
+int		push_a(t_a *b, t_a *a, size_t len);
+/* parsing_bonus.c */
+char	*ft_jointab(char **tab, size_t i, int length);
+/* rotate_bonus.c */
+void	rotate(t_a *tab, size_t len);
+void	reverse_rotate(t_a *tab, size_t len);
+void	r_and_b(t_a *a, t_a *b, size_t len_a, size_t len_b);
+void	reverse_r_and_b(t_a *a, t_a *b, size_t len_a, size_t len_b);
+
+#endif
